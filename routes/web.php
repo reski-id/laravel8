0a19cfbcb8fd1be80b8abaf2a0b593cdef635f8c
reski-id <pesan.reski@gmail.com>
2022-08-13 16:57:50 +0700

    post

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,41 @@ Route::get('/debug', function () {
 
     dd($dataarray);
 });
+
+$tasklist =[
+    1 => 'eat',
+    2 => 'study',
+    3 => 'work'
+];
+//get all tasklist
+Route::get('/task', function () use($tasklist) {
+    return $tasklist;
+});
+
+//get tasklist with parameter
+Route::get('/task/{param}', function ($param) use($tasklist) {
+    return $tasklist[$param];
+});
+
+//searching by id
+Route::get('/task', function () use($tasklist) {
+    if (request()->search) {
+        return $tasklist[request()->search];
+    }
+    return $tasklist;
+});
+
+
+$tasklist =[
+    1 => 'eat',
+    2 => 'study',
+    3 => 'work'
+];
+Route::post('/task',  function () use($tasklist) {
+    //add csrf token di file Http\Middleware\VerifyCsrfToken
+    /*protected $except = [
+        '/task'
+    ];*/
+    $tasklist[request()->label]=request()->task;
+    return $tasklist;
+});
