eb1e234ae5c47d5cd0e06076e1e075d46e68f183
reski-id <pesan.reski@gmail.com>
2022-08-16 15:13:00 +0700

    middleware auth

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     //get data using Eloquent
     public function index(Request $request)
     {
